866f08f8d69c7f68a205d596ccc239c1bebdb3f6
Roman Storm <roman@peppersec.com>
2020-03-12 07:33:53 -0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## How to run
+edit .env
+```
+RPC_URL=https://mainnet.infura.io/v3/YOUR_KEY
+PRIVATE_KEY=
+GAS_PRICE=161
+BUMP_NONCE_TO=250
+```
---
+++ submit.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const Web3 = require('web3')
+const { numberToHex, toHex, toWei } = require('web3-utils')
+
+const {
+  PRIVATE_KEY,
+  GAS_PRICE,
+  RPC_URL,
+  BUMP_NONCE_TO
+} = process.env
+
+const web3 = new Web3(RPC_URL, null, { transactionConfirmationBlocks: 1 })
+const account = web3.eth.accounts.privateKeyToAccount('0x' + PRIVATE_KEY)
+web3.eth.accounts.wallet.add('0x' + PRIVATE_KEY)
+web3.eth.defaultAccount = account.address
+
+async function main() {
+  let nonce = await web3.eth.getTransactionCount(account.address)
+  console.log('nonce', nonce)
+  for(let i = nonce; i < BUMP_NONCE_TO; i++) {
+    try {
+      const tx = {
+        from: web3.eth.defaultAccount,
+        value: '0x00',
+        gas: numberToHex(500000), // please make sure its above `MINIMUM_CONSUMER_GAS_LIMIT` in your Oracle.sol
+        gasPrice: toHex(toWei(GAS_PRICE, 'gwei')),
+        to: web3.eth.defaultAccount,
+        netId: 42,
+        // data,
+        nonce
+      }
+      let signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
+      let result
+      if (i % 50 === 0) {
+        result = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+        console.log(`A new successfully sent tx ${result.transactionHash}`)
+      } else {
+        result = web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+        result.once('transactionHash', function(txHash){
+          console.log(`A new successfully sent tx ${txHash}`)
+        }).on('error', async function(e){
+          console.log('error', e.message)
+        })
+      }
+      nonce++
+    } catch(e) {
+      console.error('skipping tx ', txs[i], e)
+      continue
+    }
+  }
+}
+
+main()
